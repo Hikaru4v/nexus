35bf3a01b45f55c38c27df051e62a007691d279d
USForeign Policy <hikaru4v4@gmail.com>
2026-03-25 04:03:28 -0700

    Update an_nexus.js

--- an_nexus.js
+++ an_nexus.js
@@ -1,17 +1,17 @@
 {
   "sourceName": "Anime Nexus",
-  "iconUrl": "https://anime.nexus/favicon.ico",                    // You can replace with a better icon if you want
+  "iconUrl": "https://anime.nexus/favicon.ico",
   "author": {
     "name": "EnbyEnemy",
     "icon": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3122kQwublLkZ6rf1fEpUP79BxZOFmH9BSA&s"
   },
-  "version": "1.0.0",
+  "version": "1.0.1",
   "language": "English (SUB)",
   "streamType": "HLS",
   "quality": "1080p",
   "baseUrl": "https://anime.nexus/",
   "searchBaseUrl": "https://anime.nexus/api/search?q=%s&limit=20",
-  "scriptUrl": "https://github.com/Hikaru4v/nexus/raw/main/nexus.js",   // Update after upload
+  "scriptUrl": "https://github.com/Hikaru4v/nexus/raw/main/nexus.js",
   "type": "anime",
   "asyncJS": true,
   "softsub": true,
