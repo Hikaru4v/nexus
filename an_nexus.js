f5da04b58ed70d23e6d239f381d4258ca667bd0d
USForeign Policy <hikaru4v4@gmail.com>
2026-03-25 04:34:45 -0700

    Update an_nexus.js

--- an_nexus.js
+++ an_nexus.js
@@ -4,11 +4,12 @@
   "author": {
     "name": "Tobias"
   },
-  "version": "1.0.1",
+  "version": "1.0.0",
   "language": "English",
   "streamType": "HLS",
   "quality": "1080p",
   "baseUrl": "https://anime.nexus",
+  "searchBaseUrl": "https://api.anime.nexus/api/anime/shows?search=%s&sortBy=name%20asc&page=1&includes%5B%5D=poster&includes%5B%5D=genres&hasVideos=1",
   "scriptUrl": "https://raw.githubusercontent.com/Hikaru4v/nexus/main/nexus.js",
   "type": "anime",
   "asyncJS": true
