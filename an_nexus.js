fda67f7b5007366f5b79c7cc48d45789ed52e04d
USForeign Policy <hikaru4v4@gmail.com>
2026-03-25 04:00:05 -0700

    Create an_nexus.js

---
+++ an_nexus.js
@@ -0,0 +1,23 @@
+{
+  "sourceName": "Anime Nexus",
+  "iconUrl": "https://anime.nexus/favicon.ico",                    // You can replace with a better icon if you want
+  "author": {
+    "name": "EnbyEnemy",
+    "icon": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3122kQwublLkZ6rf1fEpUP79BxZOFmH9BSA&s"
+  },
+  "version": "1.0.0",
+  "language": "English (SUB)",
+  "streamType": "HLS",
+  "quality": "1080p",
+  "baseUrl": "https://anime.nexus/",
+  "searchBaseUrl": "https://anime.nexus/api/search?q=%s&limit=20",
+  "scriptUrl": "https://github.com/Hikaru4v/nexus/raw/main/nexus.js",   // Update after upload
+  "type": "anime",
+  "asyncJS": true,
+  "softsub": true,
+  "downloadSupport": true,
+  "supportsMojuru": true,
+  "supportsDartotsu": true,
+  "supportsSora": true,
+  "supportsLuna": true
+}
