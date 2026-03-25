d38c9dd284142c369be20de22de9d99e242914bb
USForeign Policy <hikaru4v4@gmail.com>
2026-03-25 04:19:14 -0700

    Update an_nexus.js

--- an_nexus.js
+++ an_nexus.js
@@ -10,16 +10,13 @@
   "quality": "1080p",
   "baseUrl": "https://anime.nexus",
   "searchBaseUrl": "https://api.anime.nexus/api/anime/shows?search=%s&sortBy=name%20asc&page=1&includes%5B%5D=poster&includes%5B%5D=genres&hasVideos=1",
-  "scriptUrl": "https://github.com/Hikaru4v/nexus/raw/main/nexus.js",
+  "scriptUrl": "https://raw.githubusercontent.com/Hikaru4v/nexus/refs/heads/main/nexus.js",
   "type": "anime",
   "asyncJS": true,
   "softsub": true,
-  "downloadSupport": false,
+  "downloadSupport": true,
   "supportsMojuru": true,
   "supportsDartotsu": true,
   "supportsSora": true,
   "supportsLuna": true
 }
-
-
-
